512883c8bb54dba11f7f23830738358d10e612d3
alabiabiodun <alabigeomatic@gmail.com>
2022-04-07 12:48:57 +0100

    Update keylessCar.js

--- keylessCar.js
+++ keylessCar.js
@@ -1,41 +1,51 @@
 
 // 1. Keyless car using function declaration with prompt()
 function checkDriverAge(){
-    var age = prompt("What is your age?");
-    if (Number(age) < 18) {
-        alert("Sorry, you are too young to drive this car. Powering off");
-    } else if (Number(age) > 18) {
-        alert("Powering On. Enjoy the ride!");
-    } else if (Number(age) === 18) {
-        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    var age = Number(prompt("What is your age?"));
+    var msg = " ";
+    if (age < 18) {
+        msg = "Sorry, you are too young to drive this car. Powering off";
+    } else if (age > 18) {
+        msg = "Powering On. Enjoy the ride!";
+    } else if (age === 18) {
+        msg = "Congratulations on your first year of driving. Enjoy the ride!";
     }
+    return alert(msg);
 }
-// Call the above function using: checkDriverAge();
+
+checkDriverAge();
 
 
 // 2. Keyless car using function expression with prompt()
-var checkDriverAge = function(){
-    var age = prompt("What is your age?");
-    if (Number(age) < 18) {
-        alert("Sorry, you are too young to drive this car. Powering off");
-    } else if (Number(age) > 18) {
-        alert("Powering On. Enjoy the ride!");
-    } else if (Number(age) === 18) {
-        alert("Congratulations on your first year of driving. Enjoy the ride!");
+var checkDriverAge2 = function(){
+    var age = Number(prompt("What is your age?"));
+    var msg = " ";
+    if (age < 18) {
+        msg = "Sorry, you are too young to drive this car. Powering off";
+    } else if (age > 18) {
+        msg = "Powering On. Enjoy the ride!";
+    } else if (age === 18) {
+        msg = "Congratulations on your first year of driving. Enjoy the ride!";
     }
+    return alert(msg);
 }
-// Call the above function using: checkDriverAge();
+
+checkDriverAge2();
 
 
 
 // 3. Keyless car using function declaration with console.log()
-function checkDriverAge(age){
-    if (Number(age) < 18) {
-        return "Sorry, you are too young to drive this car. Powering off";
-    } else if (Number(age) > 18) {
-        return "Powering On. Enjoy the ride!";
-    } else if (Number(age) === 18) {
-        return "Congratulations on your first year of driving. Enjoy the ride!";
+function checkDriverAge3(age){
+    age = Number(age);
+    var msg = " ";
+    if (age < 18) {
+        msg = "Sorry, you are too young to drive this car. Powering off";
+    } else if (age > 18) {
+        msg = "Powering On. Enjoy the ride!";
+    } else if (age === 18) {
+        msg = "Congratulations on your first year of driving. Enjoy the ride!";
     }
+    return msg;
 }
-// Call the above function using: e.g. checkDriverAge(98)
+
+console.log(checkDriverAge3(98));
